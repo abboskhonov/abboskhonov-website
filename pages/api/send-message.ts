b261ac9befbd0f6d528c8b58ded246ba0d7466b6
developer2520 <berkino2099@gmail.com>
2025-08-31 11:36:32 +0500

    small fix

--- pages/api/send-message.ts
+++ pages/api/send-message.ts
@@ -21,7 +21,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 📩 *New Contact Form Message*
 
 👤 *Name:* ${name}
-📧 ${telegram}
+📧 *@*${telegram}
 
 💬 *Message:*
     ${message}
